2882af2197bc36026c98856481a0658f87a221f4
Bobby <bobby@mantoni.me>
2015-08-08 19:06:07 -0400

    rest api calls to hue
    end-to-end working, using python-requests, to hue api. can turn on and
    off lights, and talk to motion sensor

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import time
+import requests
+import json
 import RPi.GPIO as io
+
 io.setmode(io.BCM)
 
 # Constants
@@ -9,6 +12,14 @@ pir_pin = 18
 lights_off_delay = 10
 lights_on = False
 activate_delay = 0.5
+
+bridge_ip = "192.168.1.8"
+hue_user = "2f59282530c009273ae837f135627c53"
+
+hue_url_pattern = "http://{0}/api/{1}/{2}"
+
+hue_json_on = """{{"on":{0}}}"""
+hue_json_color = """{{"on":true, "hue":{0}, "sat":{1}, "bri":{2}}}"""
 
 # configure input
 io.setup(pir_pin, io.IN)
@@ -25,7 +36,28 @@ def turnLightsOff():
     print "Turning lights off!"
     return False
 
+def getLightStatus():
+    url = hue_url_pattern.format(bridge_ip, hue_user, "lights/")
+    response = requests.get(url)
+    print response
+    print response.text
+
+def turnLightOn(id, h, s, b):
+    print "TODO"
+
+def turnLightOff(id):
+    url = hue_url_pattern.format(bridge_ip, hue_user, "lights/")
+    url += "{0}/state".format(id)
+    print url
+    j = { 'on': False }
+    response = requests.put(url, data=json.dumps(j))
+    print response
+    print response.text
+
 # main
+
+getLightStatus()
+turnLightOff(2)
 
 while True:
     if io.input(pir_pin):
