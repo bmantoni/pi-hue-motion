a0ad8cea4ce0fa8f1e8473f39ba142cd7303b6e6
Bobby Mantoni <bobby@mantoni.me>
2015-08-11 13:30:18 +0000

    adding support for groups and scenes

--- program.py
+++ program.py
@@ -16,10 +16,16 @@ activate_delay = 0.2
 bridge_ip = "192.168.1.8"
 hue_user = "2f59282530c009273ae837f135627c53"
 
+group = "nursery"
+scene_deep_sea = "726af216a-on-0"
+
+# URL Patterns
+
 hue_url_pattern = "http://{0}/api/{1}/{2}"
 
-hue_json_on = """{{"on":{0}}}"""
-hue_json_color = """{{"on":true, "hue":{0}, "sat":{1}, "bri":{2}}}"""
+hue_url_lights = "lights/"
+hue_url_groups = "groups/"
+hue_url_set_group_state = hue_url_groups + "{0}/action"
 
 # configure input
 io.setup(pir_pin, io.IN)
@@ -30,7 +36,9 @@ t0 = time.time() - lights_off_delay - 1
 # procedures
 def turnLightsOn():
     print "Turning lights on!"
-    toggleLightOnOff(2, True)
+    #toggleLightOnOff(2, True)
+    #setLightColor(2, 10000, 255, 255)
+    startScene(group, scene_deep_sea)
     return True
 
 def turnLightsOff():
@@ -45,16 +53,33 @@ def getLightStatus():
     print response.text
 
 def setLightColor(id, h, s, b):
-    print "TODO"
+    url = hue_url_pattern.format(bridge_ip, hue_user, "lights/")
+    url += "{0}/state".format(id)
+    print url
+    j = { 'on': True, 'sat':s, 'bri':b, 'hue':h }
+    response = requests.put(url, data=json.dumps(j))
+    print response
+    print response.text
+
 
 def toggleLightOnOff(id, on_off):
-    url = hue_url_pattern.format(bridge_ip, hue_user, "lights/")
+    url = hue_url_pattern.format(bridge_ip, hue_user, hue_url_lights)
     url += "{0}/state".format(id)
     print url
     j = { 'on': on_off }
     response = requests.put(url, data=json.dumps(j))
     print response
     print response.text
+
+def startScene(group, scene):
+    url = hue_url_pattern.format(bridge_ip, hue_user, 
+        hue_url_set_group_state.format(group))
+    print url
+    j = { 'on': True, 'scene': scene }
+    response = requests.put(url, data=json.dumps(j))
+    print response
+    print response.text
+
 
 # main
 
